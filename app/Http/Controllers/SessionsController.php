e6e0ed724dfc6e59b8f329bb3b3ebae390c97729
Khercog <kevin.hercog@davinci.edu.ar>
2023-02-05 00:44:48 -0300

    register, login and logout

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Nette\Schema\ValidationException;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        // Validate the request
+            $attributes = request()->validate([
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+        // Attempt to authenticate and log in the user
+        // based on the provided credentials
+
+        if (auth()->attempt($attributes)) {
+            session()->regenerate();
+            // redirect with a success flash message
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        // autn failed.
+        throw ValidationException::withMessage([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+
+    }
+
+    public function destroy()
+    {
+        auth()->logout();
+
+        return redirect('/')->with('success', 'Goodbye!');
+    }
+}
